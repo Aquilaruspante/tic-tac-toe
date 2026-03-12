1c53e3d0a8bf21cfe63882c02c320ec3cf644f35
Aquilaruspante <calogero.salvaggio@outlook.it>
2026-03-12 14:59:57 +0000

    add resetGame function

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -7,9 +7,16 @@ function randomizeActivePlayer() {
     return index;
 }
 
+const matrix = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
+
 export default function MainPage() {
     const [activePlayer, setActivePlayer] = useState();
     const [gameInitialized, setGameInitialized] = useState(false);
+    const [gameMatrix, setMatrix] = useState(matrix);
 
     function manageNewGameClick() {
         if (!gameInitialized) {
@@ -20,14 +27,26 @@ export default function MainPage() {
         };
     };
 
+    function manageRestart() {
+        setMatrix([
+            [null, null, null],
+            [null, null, null],
+            [null, null, null],
+        ]);
+        const playerIndex = randomizeActivePlayer();
+        if (playerIndex === 0) setActivePlayer('O');
+        if (playerIndex === 1) setActivePlayer('X');
+    };
+
     return (
         <>
             <header className={styles.header}>
                 <h1 className={styles.title}>Tic Tac Toe</h1>
             </header>
             <div className={styles.infoPanel}>{activePlayer ? `Active Player: ${activePlayer}` : 'Start new game'}</div>
-            <PlayArea activePlayer={activePlayer} setActivePlayer={setActivePlayer} gameInitialized={gameInitialized} />
+            <PlayArea activePlayer={activePlayer} setActivePlayer={setActivePlayer} gameInitialized={gameInitialized} gameMatrix={gameMatrix} setMatrix={setMatrix} />
             <button className={styles.button} onClick={manageNewGameClick}>New Game</button>
+            <button className={styles.restartButton} onClick={manageRestart}>Restart</button>
         </>
     )
 }
--- src/components/playArea/PlayArea.jsx
+++ src/components/playArea/PlayArea.jsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
 import Cell from "../cell/Cell";
 import styles from './PlayArea.module.css';
 
-const matrix = [
-            [null, null, null],
-            [null, null, null],
-            [null, null, null],
-        ];
-
-export default function PlayArea({ activePlayer, setActivePlayer, gameInitialized }) {
-    const [gameMatrix, setMatrix] = useState(matrix);
+export default function PlayArea({ activePlayer, setActivePlayer, gameInitialized, gameMatrix, setMatrix }) {
    
     return (
         <ul className={styles.box}>
